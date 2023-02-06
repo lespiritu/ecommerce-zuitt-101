9beb2705698a5e72d82c72f9543cfa8d1c9c5b53
espirituleoncio7 <espirituleoncio7@gmail.com>
2023-02-06 11:36:12 +0800

    updated code

--- Controllers/product-controllers/addProduct.js
+++ Controllers/product-controllers/addProduct.js
@@ -6,37 +6,65 @@ module.exports.addProduct = (request, response)=>{
     const input = request.body;
     const userData = auth.decode(request.headers.authorization);
 
-    Product.findOne({productName:input.productName})
-    .then(result => {
-        if(result === null){
-            User.findById(userData._id)
-            .then(result => {
-                  if(result.isAdmin){
-                        let newProduct = new Product(
-                            {
-                                productName:input.productName,
-                                productDescription:input.productDescription,
-                                category:input.category,
-                                price:input.price,
-                                stocks:input.stocks
-                            }
-                        )
+    if(!userData.isAdmin){
+        response.send(`You are not authorize to add product!`)
+    }
+    else{
+        Product.findOne({productName:input.productName})
+        .then(result =>{
+            if(result !== null){
+                response.send(`${result.productName} is already in the database!`)
+            }
+            else{
+                let newProduct = new Product(
+                    {
+                        productName:input.productName,
+                        productDescription:input.productDescription,
+                        category:input.category,
+                        price:input.price,
+                        stocks:input.stocks
+                    }
+                )
+    
+                // This will add and save the new product to database product collection
+                return newProduct.save()
+                .then(result => response.send(`${result.productName} is now on the products collection`))
+            }
+        })
+        .catch(error => response.send(error))
+    }
+
+    // Product.findOne({productName:input.productName})
+    // .then(result => {
+    //     if(result === null){
+    //         User.findById(userData._id)
+    //         .then(result => {
+    //               if(result.isAdmin){
+    //                     let newProduct = new Product(
+    //                         {
+    //                             productName:input.productName,
+    //                             productDescription:input.productDescription,
+    //                             category:input.category,
+    //                             price:input.price,
+    //                             stocks:input.stocks
+    //                         }
+    //                     )
             
-                        // This will add and save the new product to database product collection
-                        return newProduct.save()
-                        .then(result => response.send(`${result.productName} is now on the products collection`))
-                    }
-                    else{
-                        response.send(`You are not authorize to add product!`)
-                    }
-            })
-            .catch(error=>response.send(error))
-        }
-        else{
-            response.send(`${result.productName} is already in the database!`)
-        }
-    })
-    .catch(error=>response.send(error))
+    //                     // This will add and save the new product to database product collection
+    //                     return newProduct.save()
+    //                     .then(result => response.send(`${result.productName} is now on the products collection`))
+    //                 }
+    //                 else{
+    //                     response.send(`You are not authorize to add product!`)
+    //                 }
+    //         })
+    //         .catch(error=>response.send(error))
+    //     }
+    //     else{
+    //         response.send(`${result.productName} is already in the database!`)
+    //     }
+    // })
+    // .catch(error=>response.send(error))
     
 
 }
--- Controllers/product-controllers/updateProduct.js
+++ Controllers/product-controllers/updateProduct.js
@@ -7,34 +7,26 @@ const auth = require('../../auth.js');
 module.exports.updateProduct = (request, response)=>{
     const input = request.body;
     const userData = auth.decode(request.headers.authorization);
+    const productId = request.params.productId;
 
-    User.findById(userData._id)
-    .then(result => {
-        if(result.isAdmin){
-            
-            Product.findByIdAndUpdate(request.params.productId, 
-                {
-                    productName:input.productName, 
-                    productDescription:input.productDescription,
-                    category:input.category,
-                    isActive:input.isActive,
-                    stocks:input.stocks,
-                    price:input.price
-                    
-                   
-                },  
-                {new:true})
-                
-            .then(result => response.send(result))
-            .catch(error=> response.send(error))
-        }
-        else{
-            response.send(`You are not allowed to update product information!`)
-        }
-    })
-    .catch(error=> {
-        response.send(error);
-        response.send(`Or Incorrect product ID`)
-    })
+    let toUpdateProduct = {
+        productName:input.productName, 
+        productDescription:input.productDescription,
+        category:input.category,
+        isActive:input.isActive,
+        stocks:input.stocks,
+        price:input.price
+    }
+
+    if(userData.isAdmin){
+        Product.findByIdAndUpdate(productId, toUpdateProduct, {new:true})           
+        .then(result => response.send(result))
+        .catch(error=> response.send(error))
+    }
+    
+    else{
+        response.send(`You are not allowed to update product information!`)
+    }
+    
 }
 

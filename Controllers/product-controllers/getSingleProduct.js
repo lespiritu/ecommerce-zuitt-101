a88ab2bfd5bc3d46d337ef6be1cfd8704c67df82
espirituleoncio7 <espirituleoncio7@gmail.com>
2023-02-06 16:24:57 +0800

    added code for get single product for admin and all users

--- Controllers/product-controllers/getSingleProduct.js
+++ Controllers/product-controllers/getSingleProduct.js
@@ -1,11 +1,12 @@
 const Product = require('../../Models/productsSchema.js');
+const auth = require('../../auth.js');
 
-
-module.exports.getSingleProduct = (request, response)=>{
+// this is for getting active product only
+module.exports.getSingleActiveProduct = (request, response)=>{
 
     Product.findById(request.params.id)
     .then(result => {
-        if(result !== null){
+        if(result !== null && result.isActive === true){
             response.send(result)
         }
         else{
@@ -13,4 +14,25 @@ module.exports.getSingleProduct = (request, response)=>{
         }
     })
     .catch(error=> response.send(`Doesn't have data from id or product id is incorect! ${error}`))
+}
+
+
+// this will get active or inactive product access by admin only
+
+module.exports.getSingleProduct = (request, response)=>{
+
+    const productId = request.params.productId;
+    const userData  = auth.decode(request.headers.authorization);
+
+    if(userData.isAdmin){
+        Product.findById(productId)
+        .then(result => {
+            response.send(result)
+        })
+        .catch(error => response.send(error))
+    }
+    else{
+        response.send("You don't have permission to this page!")
+    }
+    
 }
--- Routes/productRoutes.js
+++ Routes/productRoutes.js
@@ -7,17 +7,18 @@ const auth = require('../auth.js')
 
 const {addProduct} = require('../Controllers/product-controllers/addProduct.js');
 const {getAllActiveProducts} = require('../Controllers/product-controllers/getAllActiveProducts.js');
-const {getSingleProduct} = require('../Controllers/product-controllers/getSingleProduct.js');
+const {getSingleActiveProduct, getSingleProduct} = require('../Controllers/product-controllers/getSingleProduct.js');
 const {updateProduct} = require('../Controllers/product-controllers/updateProduct.js');
 const { getAllProducts, getInActiveProducts} = require('../Controllers/product-controllers/getAllProducts.js');
 
 // ============= all request route here ===============================================
 router.post('/addProduct',auth.verify, addProduct);
 router.get('/allActiveProducts', getAllActiveProducts);
-router.get('/productId/:id',getSingleProduct );
+router.get('/productIdActive/:id',getSingleActiveProduct );
+
 router.put('/update/:productId', auth.verify, updateProduct);
 router.get('/allProducts',auth.verify, getAllProducts );
 router.get('/inActiveProducts', auth.verify, getInActiveProducts)
-
+router.get('/productId/:productId', auth.verify, getSingleProduct)
 
 module.exports = router;

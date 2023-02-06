414380c79436db6153913d61b41520cd15ad5664
espirituleoncio7 <espirituleoncio7@gmail.com>
2023-02-06 21:59:33 +0800

    added functionality to add to cart

--- Controllers/cart-controllers/addToCart.js
+++ Controllers/cart-controllers/addToCart.js
@@ -15,46 +15,53 @@ module.exports.addToCart = (request, response)=>{
     }
     else{
 
-        Product.findById(productId)
-        .then(result => {
-            if(result === null){
-                response.send("Invalid productID!")
+        Cart.findOneAndUpdate( {$and:[{userId: userData._id  }, {productId: productId}]},{$inc:{quantity:input.quantity? input.quantity : 1} } , {new:true})
+        .then(data => {
+            if(data !== null){
+               data.totalAmount = data.price * data.quantity;
+               data.save()
+
+               .then(newData =>response.send(newData) )
+               .catch(error => respond.send(error))
             }
-       
             else{
-
-                // Cart.findOne( {$and:[{userId: userData._id}, {productId:result._id } ] })
-                // .then(result => {
-                //     if (result === null){
-
-                //     }
-                // })
-                let newProductOnCart = new Cart(
-                    {   
-
-                        userId: userData._id,
-                        userEmail: userData.email,
-
-                        productId: result._id,
-                        productName: result.productName,
-                        productDescription: result.productDescription,
-                        price: result.price,
-                        quantity: input.quantity? input.quantity : 1,
-                        totalAmount:input.quantity? result.price * input.quantity : result.price * 1
-                            
-                        
-                        
+                Product.findById(productId)
+                .then(result => {
+                    if(result === null){
+                        response.send("Invalid productID!")
                     }
-                )
+            
+                    else{
+                        let newProductOnCart = new Cart(
+                            {   
+
+                                userId: userData._id,
+                                userEmail: userData.email,
+
+                                productId: result._id,
+                                productName: result.productName,
+                                productDescription: result.productDescription,
+                                price: result.price,
+                                quantity: input.quantity? input.quantity : 1,
+                                totalAmount:input.quantity? result.price * input.quantity : result.price * 1
+                                    
+                                
+                                
+                            }
+                        )
 
 
-                // This will add and save the new product in the cart to database cart collection
-                return newProductOnCart.save()
-                .then(result => response.send(`${result.productName} is now on your cart`))
-                .catch(error=>response.send(error))
+                        // This will add and save the new product in the cart to database cart collection
+                        return newProductOnCart.save()
+                        .then(result => response.send(`${result.productName} is now on your cart`))
+                        .catch(error=>response.send(error))
+                    }
+                })
+                .catch(error => response.send(error))
             }
         })
-        .catch(error => response.send(error))
+
+        .catch(error=> response.send(error))
 
         
     }

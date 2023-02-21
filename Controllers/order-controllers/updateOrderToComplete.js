a2377fed1bf998782d9249387df9151db6a9036f
espirituleoncio7 <espirituleoncio7@gmail.com>
2023-02-21 21:55:43 +0800

    edit response for update to complete order

--- Controllers/order-controllers/updateOrderToComplete.js
+++ Controllers/order-controllers/updateOrderToComplete.js
@@ -7,36 +7,56 @@ module.exports.updateOrderToComplete = (request, response)=>{
     const orderId = request.params.orderId;
 
     if(userData.isAdmin){
-        response.send("This page is restricted for user only. Admin doesn't have an access!");
+        response.send({
+            "status":"failed",
+            "message":"Admin cannot access this page!"
+        });
     }
     else{
         Order.findById(orderId)
         .then(data => {
             if (data === null){
-                response.send("Invalid Order ID")
+                response.send({
+                    "status":"failed",
+                    "message":"Invalid Order ID"
+                })
             }
             else{
 
                 if(data.orderStatus === "recieved"){
-                    return response.send("This product is already recieved")
+                    return response.send({
+                        "status":"failed",
+                        "message":"This product is already recieved"
+                    })
                 }
                 else{
                     if(data.userId === userData._id){
 
                         data.orderStatus = "recieved";
                         data.save()
-                        .then( ()=> response.send(`OrderNo: ${data._id} is now recieved and completed. Thank you!`) )
+                        .then( ()=> response.send({
+                            "status":"success",
+                            "message":"Order completed!",
+                            data
+                        }) )
                         .catch(error => response.send(error))
                     }
                     else{
-                        response.send("You don't have permission to this page!")
+                        response.send({
+                            "status":"failed",
+                            "message":"You don't have permission to this page!"
+                        })
                     }
                 }
                 
                 
             }
         })
-        .catch(error => response.send(error))
+        .catch(error =>response.send({
+            "status":"failed",
+            "message":"Error",
+            error
+        }))
     }
 }
 
